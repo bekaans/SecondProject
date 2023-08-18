e60c097e0242d5ef85c20649417acff45818def6
Berke Kaan <bekaans@icloud.com>
2023-08-18 16:27:57 +0300

    Add DALOgrenci => OgrenciEkle

---
+++ DataAccessLayerr/DALOgrenci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using EntityLayer;
+namespace DataAccessLayer
+{
+    public class DALOgrenci
+    {
+        public static int OgrenciEkle(EntityOgrenci parametre)
+        {
+            SqlCommand komut1 = new SqlCommand("insert into TblOgrenci(OgrenciAd,OgrenciSoyad,OgrenciNumara,OgrenciMail,OgrenciSifre) values (@p1,@p2,@p3,@p4,@p5)",SqlBaglanti.sql);
+            if (komut1.Connection.State != ConnectionState.Open) {
+                komut1.Connection.Open();
+            }
+            komut1.Parameters.AddWithValue("@p1", parametre.AD);
+            komut1.Parameters.AddWithValue("@p2", parametre.SOYAD);
+            komut1.Parameters.AddWithValue("@p3", parametre.NUMARA);
+            komut1.Parameters.AddWithValue("@p4", parametre.FOTOGRAF);
+            komut1.Parameters.AddWithValue("@p5", parametre.SIFRE);
+            return komut1.ExecuteNonQuery();
+        }
+    }
+}
