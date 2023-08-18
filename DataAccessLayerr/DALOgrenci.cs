23fac273f910e7c83edaf094a5ca7d9dea2955bc
Berke Kaan <bekaans@icloud.com>
2023-08-18 18:29:00 +0300

    Add DALOgrenci SqlDataReader and List methods

--- DataAccessLayerr/DALOgrenci.cs
+++ DataAccessLayerr/DALOgrenci.cs
@@ -12,7 +12,7 @@ namespace DataAccessLayer
     {
         public static int OgrenciEkle(EntityOgrenci parametre)
         {
-            SqlCommand komut1 = new SqlCommand("insert into TblOgrenci(OgrenciAd,OgrenciSoyad,OgrenciNumara,OgrenciMail,OgrenciSifre) values (@p1,@p2,@p3,@p4,@p5)",SqlBaglanti.sql);
+            SqlCommand komut1 = new SqlCommand("insert into TblOgrenci(OgrAd,OgrSoyad,OgrNumara,OgrFotograf,OgrSifre) values (@p1,@p2,@p3,@p4,@p5)",SqlBaglanti.sql);
             if (komut1.Connection.State != ConnectionState.Open) {
                 komut1.Connection.Open();
             }
@@ -22,6 +22,30 @@ namespace DataAccessLayer
             komut1.Parameters.AddWithValue("@p4", parametre.FOTOGRAF);
             komut1.Parameters.AddWithValue("@p5", parametre.SIFRE);
             return komut1.ExecuteNonQuery();
+        }
+        public static List<EntityOgrenci> OgrenciListesi()
+        {
+            List<EntityOgrenci> degerler = new List<EntityOgrenci>();
+            SqlCommand komut2 = new SqlCommand("Select * from TblOgrenciler", SqlBaglanti.sql);
+            if (komut2.Connection.State != ConnectionState.Open)
+            {
+                komut2.Connection.Open();
+            }
+            SqlDataReader sqlDataReader = komut2.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                EntityOgrenci entity = new EntityOgrenci();
+                entity.ID = Convert.ToInt32(sqlDataReader["OGRID"]);
+                entity.AD = sqlDataReader["OgrAd"].ToString(); 
+                entity.SOYAD = sqlDataReader["OgrSoyad"].ToString(); 
+                entity.NUMARA = sqlDataReader["OgrNumara"].ToString(); 
+                entity.FOTOGRAF = sqlDataReader["OgrFotograf"].ToString(); 
+                entity.SIFRE = sqlDataReader["OgrSifre"].ToString(); 
+             
+                degerler.Add(entity);
+            }
+            sqlDataReader.Close();
+            return degerler;
         }
     }
 }
--- YazOkulu/Default.aspx
+++ YazOkulu/Default.aspx
@@ -9,27 +9,27 @@
 <form id="Form1" runat="server">
 
  <div>
-        <asp:Label ID="Label1" runat="server" Text="Ogrenci Adi "></asp:Label>
+        <asp:Label ID="txtAd" runat="server" Text="Ogrenci Adi "></asp:Label>
         <asp:TextBox ID="TextBox1" runat="server" CssClass="form-control"></asp:TextBox>
 </div>
     <br />
     <div>
-        <asp:Label ID="Label2" runat="server" Text="Ogrenci Soyadı "></asp:Label>
+        <asp:Label ID="txtSoyad" runat="server" Text="Ogrenci Soyadı "></asp:Label>
         <asp:TextBox ID="TextBox2" runat="server" CssClass="form-control"></asp:TextBox>
 </div>
      <br />
     <div>
-        <asp:Label ID="Label3" runat="server" Text=" Numara "></asp:Label>
+        <asp:Label ID="txtNumara" runat="server" Text=" Numara "></asp:Label>
         <asp:TextBox ID="TextBox3" runat="server" CssClass="form-control"></asp:TextBox>
 </div>
      <br />
     <div>
-        <asp:Label ID="Label4" runat="server" Text="Sifre  "></asp:Label>
+        <asp:Label ID="txtSifre" runat="server" Text="Sifre  "></asp:Label>
         <asp:TextBox ID="TextBox4" runat="server" CssClass="form-control"></asp:TextBox>
 </div>
      <br />
     <div>
-        <asp:Label ID="Label5" runat="server" Text="Fotoğraf "></asp:Label>
+        <asp:Label ID="txtFotograf" runat="server" Text="Fotoğraf "></asp:Label>
         <asp:TextBox ID="TextBox5" runat="server" CssClass="form-control"></asp:TextBox>
 </div>
     
@@ -39,7 +39,7 @@
     
     </div>
     
-    <asp:Button ID="Button1" runat="server" CssClass="btn-info" Text="Button" OnClick="Button1_Click" />
+    <asp:Button ID="Button1" runat="server" CssClass="btn-info" Text="Kaydet" OnClick="Button1_Click" />
 </form>
 
 </asp:Content>
