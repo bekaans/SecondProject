c6e8261894ce962bce13c5ced9afcd69ebc6a8f6
Berke Kaan <bekaans@icloud.com>
2023-08-18 16:07:43 +0300

    Create SqlBaglanti and add sqlconnection

---
+++ DataAccessLayerr/SqlBaglanti.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerr
+{
+    public class SqlBaglanti
+    {
+        public static SqlConnection sql = new SqlConnection(@"Data Source=localhost;Initial Catalog=DbYazOkulu;Integrated Security=True");
+    }
+}
