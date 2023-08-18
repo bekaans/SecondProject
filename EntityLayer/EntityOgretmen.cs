75c858a1a9fd8235d70837717b4fa34b1dd78552
Berke Kaan <bekaans@icloud.com>
2023-08-18 15:49:43 +0300

    Create EntityOgretmen and Add props,encapsulation

---
+++ EntityLayer/EntityOgretmen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EntityOgretmen
+    {
+        private int OgretmenId;
+        private string OgretmenAd;
+        private int OgretmenBrans;
+
+        public int OGRETMENID { get => OgretmenId; set => OgretmenId = value; }
+        public string OGRETMENAD { get => OgretmenAd; set => OgretmenAd = value; }
+        public int OGRETMENBRANS { get => OgretmenBrans; set => OgretmenBrans = value; }
+    }
+}
