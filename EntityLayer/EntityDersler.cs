7acdc6f511912b8e7e0a9c98fa3f437c6b2ca730
Berke Kaan <bekaans@icloud.com>
2023-08-18 15:47:42 +0300

    Add EntityLayer/EntityDersler.cs and props,encapsulation

---
+++ EntityLayer/EntityDersler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EntityDersler
+    {
+        private int DersID;
+        private int DersAdi;
+        private int DersMinKontenjan;
+        private int DersMaxKontenjan;
+
+        public int DERSID { get => DersID; set => DersID = value; }
+        public int DERSAD { get => DersAdi; set => DersAdi = value; }
+        public int DERSMINKONTENJAN { get => DersMinKontenjan; set => DersMinKontenjan = value; }
+        public int DERSMAXKONTENJAN { get => DersMaxKontenjan; set => DersMaxKontenjan = value; }
+    }
+}
