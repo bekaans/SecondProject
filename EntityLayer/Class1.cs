5aeecf6771557851cc799b4917456129aca3537b
Berke Kaan <bekaans@icloud.com>
2023-08-18 15:41:34 +0300

    Add Props and Encapsulation

---
+++ EntityLayer/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace EntityLayer
+{
+    public class Class1
+    {
+
+        private int id;
+        private string ad;
+        private string soyad;
+        private string numara;
+        private string fotograf;
+        private double bakiye;
+
+        public int ID { get => id; set => id = value; }
+        public string AD { get => ad; set => ad = value; }
+        public string SOYAD { get => soyad; set => soyad = value; }
+        public string NUMARA { get => numara; set => numara = value; }
+        public string FOTOGRAF { get => fotograf; set => fotograf = value; }
+        public double BAKIYE { get => bakiye; set => bakiye = value; }
+    }
+}
